704bbef261661807e00c7954725e06358f168c7e
名前 <>
2019-11-07 14:37:03 +0000

    rails05 kadai

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ApplicationRecord
   # format:{with:ここはインターネットからコピペ
   # emailのwith以降の内容<https://qiita.com/matsubo/items/3dfb1ddb6e155c44643b>
   
-  validates :password, length: { in: 8..32}, format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i}
-  validates :password_confirmation, length: { in: 8..32}, format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i}
+  validates :password, length: { in: 8..32}, format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
   
   has_secure_password
 end
